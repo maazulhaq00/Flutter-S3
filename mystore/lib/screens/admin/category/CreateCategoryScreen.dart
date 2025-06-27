6fd404f8129f3b6c7e97ce49d2d264ec3694d52b
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-06-27 11:44:31 +0500

    flutter firebase conn &  work

---
+++ mystore/lib/screens/admin/category/CreateCategoryScreen.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+var db = FirebaseFirestore.instance;
+
+class CreateCategoryScreen extends StatefulWidget {
+  const CreateCategoryScreen({super.key});
+
+  @override
+  State<CreateCategoryScreen> createState() => _CreateCategoryScreenState();
+}
+
+class _CreateCategoryScreenState extends State<CreateCategoryScreen> {
+  final catNameController = TextEditingController();
+
+  void addCategory() async {
+    try {
+      await db.collection("category").add({
+        "categoryName": catNameController.text,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Category Added Successfully"),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } catch (err) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error Occureed"), backgroundColor: Colors.red),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Create Category"))),
+      backgroundColor: const Color.fromARGB(255, 178, 210, 236),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: catNameController,
+              decoration: InputDecoration(
+                labelText: 'Category Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: addCategory, child: Text("Add")),
+          ],
+        ),
+      ),
+    );
+  }
+}
